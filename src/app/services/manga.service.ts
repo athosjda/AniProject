e7581b24f3a90edf7d2a8cbc3a874e2464a25bbe
athosjda <athosjda@gmail.com>
2021-12-13 17:04:12 -0300

    Implementação manga service

--- src/app/services/manga.service.ts
+++ src/app/services/manga.service.ts
@@ -1,4 +1,7 @@
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { catchError, Observable, retry, throwError } from 'rxjs';
+import { Manga } from '../models/manga';
 
 @Injectable({
   providedIn: 'root'
@@ -7,5 +10,28 @@ export class MangaService {
 
   url = 'https://api.jikan.moe/v3/anime';
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+
+  httpOptions = {
+    headers: new HttpHeaders({'Content-Type': 'application/json'})
+  }
+
+  getAnimeById(id: number): Observable<Manga>{
+    return this.httpClient.get<Manga>(this.url+'/'+id).pipe(
+      retry(2),
+      catchError(this.handleError)
+    )
+  }
+
+  handleError(error: HttpErrorResponse){
+    let errorMessage = '';
+    if(error.error instanceof ErrorEvent){
+      errorMessage = error.error.message;
+    } else {
+      errorMessage = `Código do erro: ${error.status}, ` + `menssagem: ${error.message}`;
+    }
+    console.log(errorMessage);
+    return throwError(errorMessage);
+  }
+
 }
